681cea8197e6f918aef08b953ca58080bd720f12
Bill Cui <billcui2001@hotmail.com>
2020-11-13 22:27:00 -0500

    initial

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text h1>Medimate</Text>
+      <Text h2>Medimate</Text>
+      <Text h3>Medimate</Text>
+      <Text h4>Medimate</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
